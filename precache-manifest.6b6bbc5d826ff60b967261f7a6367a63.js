f31cba2e1da7db5bad498a722082aba5f7c6eaab
Felix Gnass <fgnass@gmail.com>
2019-07-04 10:18:10 +0200

    Updates

---
+++ precache-manifest.6b6bbc5d826ff60b967261f7a6367a63.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a898ca9b288ed6ff90c4e5dc0a3cea47",
+    "url": "/roulette/index.html"
+  },
+  {
+    "revision": "50dfb079e77074fb6e74",
+    "url": "/roulette/static/js/2.a6da83c4.chunk.js"
+  },
+  {
+    "revision": "32189d7430ac5157c3b7",
+    "url": "/roulette/static/js/main.623014a1.chunk.js"
+  },
+  {
+    "revision": "ee036c026ee6bd48b1d4",
+    "url": "/roulette/static/js/runtime~main.461de433.js"
+  }
+]);
